c0c6a7bbf8e382155915395c5be00ddfc39adf68
Bullnox <sangeetaradhags@gmail.com>
2025-05-26 04:35:48 +0000

    remove

--- install.sh
+++
@@ -1,1 +0,0 @@
-pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian-lxde.sh -O debian-lxde.sh && chmod +x debian-lxde.sh && bash debian-lxde.sh
